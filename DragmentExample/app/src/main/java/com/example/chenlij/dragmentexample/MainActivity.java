084f369da06c71ff7489af494338d0d4ff5c9fc0
GIGAFortress <clj8196@126.com>
2016-08-17 19:09:12 +0800

    只要在activity_main.xml 中添加V4的DrawerLayout就可以实现抽屉的效果了， 第二个是要android:layout_gravity="start" 而不能是 android:gravity="start"

--- DragmentExample/app/src/main/java/com/example/chenlij/dragmentexample/MainActivity.java
+++ DragmentExample/app/src/main/java/com/example/chenlij/dragmentexample/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.chenlij.dragmentexample;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Fragment mLeftMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+//        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+//            @Override
+//            public void onDrawerClosed(View drawerView) {
+//                super.onDrawerClosed(drawerView);
+//            }
+//
+//            @Override
+//            public void onDrawerOpened(View drawerView) {
+//                super.onDrawerOpened(drawerView);
+//            }
+//        };
+
     }
 }
