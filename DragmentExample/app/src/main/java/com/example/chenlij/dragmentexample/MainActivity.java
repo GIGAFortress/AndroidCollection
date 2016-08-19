c43eca9627b7718b9108a828e292dbb02de66507
GIGAFortress <clj8196@126.com>
2016-08-19 15:36:01 +0800

    完成了那种汉堡箭头的动画效果

--- DragmentExample/app/src/main/java/com/example/chenlij/dragmentexample/MainActivity.java
+++ DragmentExample/app/src/main/java/com/example/chenlij/dragmentexample/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.chenlij.dragmentexample;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 
@@ -15,17 +17,26 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     private Fragment mLeftMenu;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         /*调用这个方法有个限制,即必须在setContentView(R.layout.main)之前调用,否则会抛出异常*/
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        toolbar.setTitle("TOOLBAR!!");      //ToolBar设置标题
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));     //设置标题颜色
+        getSupportActionBar().hide();       //隐藏AppCompatActivity的标题
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);       //设置阴影
+
+        /*以下这3个组合完成汉堡图标至箭号图标的动画效果*/
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -36,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 super.onDrawerOpened(drawerView);
             }
         };
-
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 }
