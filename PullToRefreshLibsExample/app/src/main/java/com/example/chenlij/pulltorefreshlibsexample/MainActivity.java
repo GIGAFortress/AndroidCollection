044be28c840573a6f72e14959f33ce8722d32db2
GIGAFortress <clj8196@126.com>
2016-08-22 17:36:58 +0800

    添加了PullToRefreshListView

--- PullToRefreshLibsExample/app/src/main/java/com/example/chenlij/pulltorefreshlibsexample/MainActivity.java
+++ PullToRefreshLibsExample/app/src/main/java/com/example/chenlij/pulltorefreshlibsexample/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.chenlij.pulltorefreshlibsexample;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PullToRefreshListView mPullRefreshListView;
+    private LinkedList<String> mListItems;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+        mPullRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+                // Update the LastUpdatedLabel
+                refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+                // Do work to refresh the list here.
+                new GetDataTask().execute();
+            }
+        });
+        mPullRefreshListView.setOnLastItemVisibleListener(new OnLastItemVisibleListener() {
+            @Override
+            public void onLastItemVisible() {
+                Toast.makeText(MainActivity.this, "End of List!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ListView actualListView = mPullRefreshListView.getRefreshableView();
+
+        // Need to use the Actual ListView when registering for Context Menu
+        registerForContextMenu(actualListView);
+
+        mListItems = new LinkedList<String>();
+        mListItems.addAll(Arrays.asList(mStrings));
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListItems);
+        actualListView.setAdapter(mAdapter);
     }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+            }
+            return mStrings;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            mListItems.addFirst("Added after refresh...");
+            mAdapter.notifyDataSetChanged();
+
+            // Call onRefreshComplete when the list has been refreshed.
+            mPullRefreshListView.onRefreshComplete();
+
+            super.onPostExecute(result);
+        }
+    }
+
+    private String[] mStrings = { "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler", "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler" };
 }
