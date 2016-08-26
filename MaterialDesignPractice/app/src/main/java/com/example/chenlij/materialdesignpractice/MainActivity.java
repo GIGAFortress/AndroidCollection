3c6079cb69b4ca59a51622d568de1f352b476e05
GIGAFortress <clj8196@126.com>
2016-08-26 16:44:14 +0800

    通过搜索添加 de.hdodenhof:circleimageview  jar包  可以使用 de.hdodenhof.circleimageview.CircleImageView 控件 该控件自身就是个圆形的imageview

--- MaterialDesignPractice/app/src/main/java/com/example/chenlij/materialdesignpractice/MainActivity.java
+++ MaterialDesignPractice/app/src/main/java/com/example/chenlij/materialdesignpractice/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.chenlij.materialdesignpractice;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationView mNavigationView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        getSupportActionBar().hide();       //隐藏AppCompatActivity的标题
+//        setSupportActionBar(mToolbar);    //这个是不能用的，会报错
+        setUpProfileImage();
+    }
+    private void setUpProfileImage() {
+        View headerView=  mNavigationView.inflateHeaderView(R.layout.navigation_header);
+        View profileView = headerView.findViewById(R.id.profile_image);
+        if (profileView != null) {
+            profileView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    switchToBlog();
+//                    mDrawerLayout.closeDrawers();
+//                    mNavigationView.getMenu().getItem(1).setChecked(true);
+                }
+            });
+        }
     }
 }
