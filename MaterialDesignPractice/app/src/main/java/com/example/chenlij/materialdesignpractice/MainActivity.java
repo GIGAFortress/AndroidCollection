724bbfe453599ffbf9455fbac974d01d66a403ad
GIGAFortress <clj8196@126.com>
2016-08-26 17:04:47 +0800

    添加 mDrawerToggle 使之打开时会有效果

--- MaterialDesignPractice/app/src/main/java/com/example/chenlij/materialdesignpractice/MainActivity.java
+++ MaterialDesignPractice/app/src/main/java/com/example/chenlij/materialdesignpractice/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.chenlij.materialdesignpractice;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,19 +13,27 @@ public class MainActivity extends AppCompatActivity {
 
     private NavigationView mNavigationView;
     private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
-//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         getSupportActionBar().hide();       //隐藏AppCompatActivity的标题
 //        setSupportActionBar(mToolbar);    //这个是不能用的，会报错
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
         setUpProfileImage();
     }
     private void setUpProfileImage() {
-        View headerView=  mNavigationView.inflateHeaderView(R.layout.navigation_header);
+        View headerView=  mNavigationView.inflateHeaderView(R.layout.navigation_header);    //NavigationView方法inflateHeaderView填充一个头部
         View profileView = headerView.findViewById(R.id.profile_image);
         if (profileView != null) {
             profileView.setOnClickListener(new View.OnClickListener() {
