498240052c50c434f1c82fd648f333e08f35c82c
GIGAFortress <clj8196@126.com>
2016-09-05 17:19:20 +0800

    代码整理

--- MaterialDesignPractice/app/src/main/java/com/example/chenlij/materialdesignpractice/news/NewsListActivity.java
+++ MaterialDesignPractice/app/src/main/java/com/example/chenlij/materialdesignpractice/news/NewsListActivity.java
@@ -6,9 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.chenlij.materialdesignpractice.R;
 import com.example.chenlij.materialdesignpractice.widget.DividerOffsetDecoration;
+import com.example.chenlij.materialdesignpractice.widget.PullToRefreshLayout;
 import com.example.chenlij.materialdesignpractice.widget.RefreshLayout;
 
 /**
@@ -19,26 +23,60 @@ public class NewsListActivity extends AppCompatActivity {
     private RefreshLayout refreshLayout;
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLayoutManager;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        refreshLayout = (RefreshLayout) findViewById(R.id.refresh_layout);
-        refreshLayout.setColorSchemeResources(R.color.google_blue,
-                R.color.google_green,
-                R.color.google_red,
-                R.color.google_yellow);
-//        refreshLayout.setOnRefreshListener(new PullToRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                Toast.makeText(NewsListActivity.this, "onRefresh", 500).show();
-//            }
-//        });
+        initToolBar();
+        initRefreshView();
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.addItemDecoration(new DividerOffsetDecoration());
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("最新");
+        setSupportActionBar(toolbar);   //Mainfest中没有设置AppTheme也不会报错
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            /*设置后ToolBar返回键作用启动*/
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void initRefreshView() {
+        refreshLayout = (RefreshLayout) findViewById(R.id.refresh_layout);
+//        assert null == refreshLayout;
+        refreshLayout.setColorSchemeResources(R.color.google_blue,
+                R.color.google_green,
+                R.color.google_red,
+                R.color.google_yellow);
+        refreshLayout.setOnRefreshListener(new PullToRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e){
+
+                }
+                Toast.makeText(NewsListActivity.this, "onRefresh", 500).show();
+            }
+        });
+        refreshLayout.setOnLoadListener(new RefreshLayout.OnLoadListener() {
+            @Override
+            public void onLoad() {
+                Toast.makeText(NewsListActivity.this, "setOnLoadListenr", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
